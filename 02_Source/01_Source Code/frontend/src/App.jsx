fa2ed662f2305808090271d47dd82b280a09fdd0
nqdoan22 <nqdoan22@clc.fitus.edu.vn>
2026-03-13 16:11:34 +0700

    Temp

--- 02_Source/01_Source Code/frontend/src/App.jsx
+++ 02_Source/01_Source Code/frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import MaterialsPage from './pages/MaterialsPage';
-import LotsPage from './pages/LotsPage';
-import QCTestsPage from './pages/QCTestsPage';
-import BatchesPage from './pages/BatchesPage';
-import LabelsPage from './pages/LabelsPage';
-import DashboardPage from './pages/DashboardPage';
-import UsersPage from './pages/UsersPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import MaterialsPage from "./pages/MaterialsPage";
+import LotsPage from "./pages/LotsPage";
+import QCTestsPage from "./pages/QCTestsPage";
+import BatchesPage from "./pages/BatchesPage";
+import LabelsPage from "./pages/LabelsPage";
+import DashboardPage from "./pages/DashboardPage";
+import UsersPage from "./pages/UsersPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="app-layout">
+        <h1>Hello World</h1>
         <Sidebar />
         <main className="main-content">
           <Routes>
